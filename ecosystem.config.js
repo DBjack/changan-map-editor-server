f02a117e402ac6714a59fd8c10e7500504ead307
王中杰 <wangzhongjie>
2026-06-17 17:23:23 +0800

    build(ci): 调整生产环境配置并新增部署相关文件
    1. 关闭生产环境的swagger开关
    2. 新增开发环境配置文件
    3. 新增nginx部署配置文件
    4. 完善github actions部署流程，新增nginx配置推送和重载逻辑

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: 'production',
-        ENABLE_SWAGGER: 'true',
+        ENABLE_SWAGGER: 'false',
       },
       error_file: './logs/pm2-err.log',
       out_file: './logs/pm2-out.log',
