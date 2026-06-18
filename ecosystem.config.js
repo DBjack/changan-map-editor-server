56d8b466697bd0b7722530fc0cf036164108d1e9
王中杰 <wangzhongjie>
2026-06-18 09:47:19 +0800

    build(config): 调整配置并启用swagger
    1.  给nginx新增监听80端口
    2.  生产环境启用swagger文档
    3.  移除pm2配置里冗余的swagger环境变量
    4.  优化tsconfig编译配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,6 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: 'production',
-        ENABLE_SWAGGER: 'false',
       },
       error_file: './logs/pm2-err.log',
       out_file: './logs/pm2-out.log',
